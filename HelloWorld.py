3c39f0050a3ae02b68857d5f233f3d890283caa7
Salaj Singh <salajsingh@yahoo.com>
2022-11-12 19:54:36 -0800

    Commit hello work file and chapter1 folder

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+
+#pip install numpy
+#pip install panda
+
+#import numpy as np
+#import panda as pd
+
+print ("start of the program")
+
+num = 7
+amt = 123.45
+code = 'A'
+pi = 3.14159
+population_of_india = 1000000000
+msg = "Hi"
+
+print ("NUM = "+str(num))
+print ("AMT = "+str(amt))
+print ("CODE = "+str(code))
+print ("PI = "+str(pi))
+print ("POPULATION_OF_INDIA = "+str(population_of_india))
+print("HI = "+str(msg))
+
+print ("End of the program")
